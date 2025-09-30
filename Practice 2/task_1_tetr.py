396948d1df8ea9e2341e3ffb93f5d5b000ef4dfe
ByySpeenyx <superzemcev@gmail.com>
2025-09-30 20:35:06 +0700

    Update task_1_tetr.py

--- Practice 2/task_1_tetr.py
+++ Practice 2/task_1_tetr.py
@@ -3,6 +3,7 @@ def tetration(grade, number):
     for i in range(grade):
         num*=number
     return number**num
-check1=str(tetration(3,5))
-check2=str(tetration(5,2))
-print(len(check1), len(check2))
+n=int(input())
+k=int(input())
+print(len(str(tetration(n,k))))
+
