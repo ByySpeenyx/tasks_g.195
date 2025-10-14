ba616304740954bd95d3d8fa4d6de1cc924bd918
ByySpeenyx <superzemcev@gmail.com>
2025-10-14 22:45:14 +0700

    Add files via upload

---
+++ Practice 3/task_10.py
@@ -0,0 +1,13 @@
+def func(s):
+    soft=[2, 3, 5, 7, 11, 13, 17]
+    r=[]
+    if s[0]!='0':
+        for i in range(1,len(s)-2):
+            b=int(s[i:i+3])
+            for j in soft:
+                if b%j==0:
+                    r.append(b)
+    return sum(r)
+s='1406357289'
+print(func(s))
+
---
+++ Practice 3/task_4.py
@@ -0,0 +1,12 @@
+def linsech(arr, tar):
+    if len(arr)==0:
+        return -1
+    if len(arr)==1 and arr[0]==tar:
+        return a[0]
+    for i in range(len(arr)):
+        if arr[i]==tar:
+            return i
+n=int(input())
+arr=[int(input()) for i in range(n)]
+tar=int(input())
+print(linsech(arr, tar))
